76d984d8f5e2117ee33ab83df70ed044cad03ca1
Chris Sprehe <chris.sprehe@gmail.com>
2014-07-03 09:00:08 -0500

    Initial commit strictly of the EAK out of the box

---
+++ bower_components/ember-qunit/lib/module-for-component.js
@@ -0,0 +1,49 @@
+import testResolver from './test-resolver';
+import moduleFor from './module-for';
+import Ember from 'ember';
+
+export default function moduleForComponent(name, description, callbacks) {
+  var resolver = testResolver.get();
+
+  moduleFor('component:' + name, description, callbacks, function(container, context, defaultSubject) {
+    var layoutName = 'template:components/' + name;
+
+    var layout = resolver.resolve(layoutName);
+
+    if (layout) {
+      container.register(layoutName, layout);
+      container.injection('component:' + name, 'layout', layoutName);
+    }
+
+    context.dispatcher = Ember.EventDispatcher.create();
+    context.dispatcher.setup({}, '#ember-testing');
+
+    context.__setup_properties__.render = function() {
+      var containerView = Ember.ContainerView.create({container: container});
+      var view = Ember.run(function(){
+        var subject = context.subject();
+        containerView.pushObject(subject);
+        // TODO: destory this somewhere
+        containerView.appendTo('#ember-testing');
+        return subject;
+      });
+
+      return view.$();
+    };
+
+    context.__setup_properties__.append = function(){
+      Ember.deprecate('this.append() is deprecated. Please use this.render() instead.');
+      return this.render();
+    };
+
+    context.$ = function(){
+      var $view = this.render(), subject = this.subject();
+
+      if(arguments.length){
+        return subject.$.apply(subject, arguments);
+      }else{
+        return $view;
+      }
+    };
+  });
+}
